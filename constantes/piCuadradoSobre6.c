1101a45172af6fc8f01f49a636ec542cc2a8e033
Ademar <ademardeleonvazquez@gmail.com>
2025-02-25 20:24:32 -0600

    pi cuadrado entre 6

---
+++ constantes/piCuadradoSobre6.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int n, i;
+    float fct=1.0;
+    printf("Cual es el numero de terminos? ");
+    scanf("%d", &n);
+    for (i = 1; i < n; i++)
+    {
+        fct +=  1.0/((i+1)*(i+1));
+    }
+    printf("\nln(2) = %f\n", fct);
+   
+    return 0;
+}
