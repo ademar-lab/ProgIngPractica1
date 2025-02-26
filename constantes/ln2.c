e953e0032901ab34cfbe1f340334808ac9bd26ee
Ademar <ademardeleonvazquez@gmail.com>
2025-02-25 20:06:19 -0600

    ln(2)

--- constantes/ln2.c
+++ constantes/ln2.c
@@ -1,7 +1,17 @@
-#include <stdio.h>;
+#include <stdio.h>
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    int n, sg, i;
+    float aux, fct=1.0;
+    printf("Cual es el numero de terminos? ");
+    scanf("%d", &n);
+    for (i = 1; i < n; i++)
+    {
+        sg = 2*((i+1)%2)-1;
+        fct +=  sg*(1.0/(i+1));
+    }
+    printf("\nln(2) = %f\n", fct);
+   
     return 0;
 }
