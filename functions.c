0348fc67fbb582c986250c90d07205c108898ce1
Ademar <ademardeleonvazquez@gmail.com>
2025-03-09 09:27:54 -0600

    Se añadió una función para calcular el numero de bernoulli y el programa tan(x).c funciona para 5 términos sin overflow

---
+++ functions.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+float calcularBernoulli(int k) {
+    int i, j;
+    float den, num=1.0, Bi, i_factorial, coef_bin_num;
+
+    // Lista de todos los números de bernoulli, desde B0 hasta Bk
+    float bList[k];
+    // B0 = 1
+    bList[0] = 1;
+
+    // Comenzar el cálculo de la serie
+    for (j = 1; j <= k; j++)
+    {
+        // Se establece a 0 para que los números de bernoulli previos no afecten a los próximos
+        Bi = 0.0;
+        coef_bin_num = 0.0;
+        i_factorial = 0.0;
+        for (i = 0; i < j; i++)
+        {
+            // El algoritmo se comporta diferente en i=0 debido a que 0! = 1 
+            coef_bin_num = coef_bin_num ? coef_bin_num*(j-i+1) : 1;
+            // Solo cambiar el numerador del coeficiente binomial si i =! 0
+            i_factorial = (i ? i_factorial*i: 1);
+            den = j+1-i;
+            // Coeficiente binomial -> k!/(i!(K-i)!) = k*(k-1)*...*(k-i+2)*(k-i+1) / i!
+            // Cuando i = 0, coeficiente binomial = 1
+            // El Bi anterior se tiene que tomar con signo negativo
+            Bi += (coef_bin_num/i_factorial)*(bList[i]/den);
+        }
+        Bi *= -1.0;
+        // Para el primer ciclo se calculará B1, que se usará en B2
+        bList[j] = Bi;
+    }
+
+    return bList[k];
+}
---
+++ trigonometricas/tan(x).c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "../functions.c"
+
+int main(int argc, char const *argv[])
+{
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     printf("El numero de bernoulli de %d es: %f\n", i, calcularBernoulli(i));
+    // }
+    
+    int n, sgn, i, factorial=1;
+    float x, den=1.0, num=1.0, fct=1.0, tanx=0.0, exp1 = 1.0, exp2 = 1.0;
+    // Preguntar por el número de términos
+    do
+    {
+        printf("Cual es el numero de terminos? ");
+        scanf("%d", &n);
+    } while (n<1);
+    do
+    {
+        printf("Cual es el valor de x(El dominio es: -1.57079 < x < 1.57079)?");
+        scanf("%f", &x);
+    } while (x < -1.57079 || x > 1.57079);
+
+    // Comenzar el cálculo de la serie
+    for (i = 1; i <= n; i++)
+    {   
+        // El factorial se comporta diferente en 0!
+        // Si se hacen más de 6 iteraciones, el factorial se hace demasiado grande
+        factorial *= (i-1) ? ((2*i)*((2*i)-1)):2;
+        sgn = (2*(i%2))-1;
+        exp1 *= -4;
+        // Siempre se busca mantener negativo exp2
+        exp2 = 1 + (sgn*exp1);
+        fct = (calcularBernoulli((2*i))*exp1*exp2)/factorial;
+        printf("fact = (%f*%f*%f)/%d", calcularBernoulli((2*i)), exp1, exp2, factorial);
+        printf("\nfact(%d) = %f\n", i, fct);
+        // Agregar 2 al exponente de x
+        x *= (x*x);
+        tanx += fct*x;
+        printf("\nIteración %d: %f\n", i, tanx);
+    }
+    printf("\ntan(%f) = %f\n", x, tanx);
+   
+    return 0;
+}
